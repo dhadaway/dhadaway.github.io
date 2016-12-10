568c829b6b771427d4bffefa4f1c3be3806763f4
djhadaway <nelliebelly@comcast.net>
2016-12-10 12:50:43 -0800

    js fix

--- js/index.js
+++ js/index.js
@@ -19,7 +19,7 @@ $(document).ready(function() {
 
 //Scrolling animation
 $(document).ready(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
+  $('a[href*=\\#]:not([href=\\#])').click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
 
       var target = $(this.hash);
